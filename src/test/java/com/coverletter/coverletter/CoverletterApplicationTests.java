a9d41730f574aacc61b799bd86acb2a10c95f623
Pim-fy <climent5373@naver.com>
2025-09-16 16:43:35 +0900

    프로젝트 생성

---
+++ src/test/java/com/coverletter/coverletter/CoverletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coverletter.coverletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
